5bde23faf77ebded9bdf393ee7ade04c6fe3c995
Nikita Baranov <nbaranov.vrn@gmail.com>
2021-01-15 10:31:26 +0300

    Deleted frandly and cups from popanmathes, write to file popanmatches

--- popanchik.py
+++ popanchik.py
@@ -10,8 +10,10 @@ def returnMatchesForPopanchik(matches, hour, mink, maxk):
     '''выбираются матчи с кефом на фаворита между 1,45 и 1,85, начнутся через 2 и более часов, не женских и не юниорских команд.'''
     popanmatches = [] 
     for line in matches:
-        if ((mink <= line["kw1"] <= maxk or mink <= line["kw2"] <= maxk) and int(line["time"][0:2]) >= hour
-                and not ((re.search(r"(\(Ж\))|(U\d{2})", line["team1"])) or (re.search(r"(\(Ж\))|(U\d{2})", line["team2"])))):
+        if all([(mink <= line["kw1"] <= maxk or mink <= line["kw2"] <= maxk),
+                (int(line["time"][0:2]) >= hour),
+                not ((re.search(r"(\(Ж\))|(U\d{2})", line["team1"])) or (re.search(r"(\(Ж\))|(U\d{2})", line["team2"]))),
+                not (re.search(r"(товар)|(убок)", line["country"]))]):
             popanmatches.append(line)
     return popanmatches
 
@@ -23,7 +25,7 @@ def getPopanPress(popanmatches):
     ligth_press = random.randint(2,3)
     if ligth_press > (len(popanmatches) - len(usedmatches)):
         print("Недостаточно попанских матчей на 2 пресса")
-        return
+        return 
 
     while a < ligth_press:
         i = random.randint(0, len(popanmatches) - 1)
@@ -49,11 +51,11 @@ def popanchik(popmatches):
 
     print(f'''Найден{ends(len(popmatches), "", "о", "о")} \
 {len(popmatches)} матч{ends(len(popmatches), "", "а", "ей")} для Попанчика ''')
-
-    for _ in range(amt_preses):
-        press = getPopanPress(popmatches)
-        if press == None: continue
-        else: popanpress.append(press)
+    if len(popmatches) > 20:
+        for _ in range(amt_preses):
+            press = getPopanPress(popmatches)
+            if press == None: continue
+            else: popanpress.append(press)
 
     return popanpress
 
--- run.py
+++ run.py
@@ -32,20 +32,29 @@ popmatches = returnMatchesForPopanchik(matches, hour, 1.4, 1.75)
 popanpress = popanchik(popmatches)
 
 with open("out.txt", "w",encoding="UTF-8") as fileout:
+    fileout.write("#ПальцемВНебо@probitybets\n\nПоддержка и благодарность:\nhttps://vk.com/app6887721_-93234960\n\n")
     if len(popanpress) < 1:
         fileout.write('Попанчик сегодня отдыхает')
+    elif len(popanpress) > 1:
+        fileout.write("Прессы от Попанчика на сегодня:\n")
+        count = 0
+        for press in popanpress:
+            count += 1
+            fileout.write(f"\nПресс {count}.\n")
+            pressWrite(press)
     else:
-        fileout.write("#ПальцемВНебо@probitybets\n\nПоддержка и благодарность:\nhttps://vk.com/app6887721_-93234960\n\n")
-        if len(popanpress) > 1:
-            fileout.write("Прессы от Попанчика на сегодня:\n")
-            count = 0
-            for press in popanpress:
-                count += 1
-                fileout.write(f"\nПресс {count}.\n")
-                pressWrite(press)
+        fileout.write("Пресс от Попанчика на сегодня:\n")
+        for press in popanpress:
+            pressWrite(press)
+
+    fileout.write('\n\nВсе "попанcкие" матчи на сегодня:')
+    for match in popmatches:
+        if match["kw1"] < match["kw2"]:
+            fileout.write(f'{match["country"]} {match["time"]} {match["team1"]} - {match["team2"]} П1 кф. {match["kw1"]} \n')
         else:
-            fileout.write("Пресс от Попанчика на сегодня:\n")
-            for press in popanpress:
-                pressWrite(press)
+            fileout.write(f'{match["country"]} {match["time"]} {match["team1"]} - {match["team2"]} П2 кф. {match["kw2"]} \n')
+
+
+
 
 print("\nРабота программы успешно завершена. \nПрогнозы добавлены в файл out.txt ")
