76bfc7a351f82943b3165eef7bcf5b37380e2514
Nikita Baranov <nbaranov.vrn@gmail.com>
2021-05-07 16:58:57 +0300

    fix filter of matches

--- moduls/read_from_flashscore.py
+++ moduls/read_from_flashscore.py
@@ -18,9 +18,9 @@ def load_matches(data):
         html = response.text.replace('<br />', '\n')
 
         
-    # with open('livescore.html', 'w', encoding="utf-8") as doc:
-    #     for line in html:
-    #         doc.write(line)
+    with open('livescore.html', 'w', encoding="utf-8") as doc:
+        for line in html:
+            doc.write(line)
     return html
 
 def html_to_dict(html):
@@ -31,14 +31,30 @@ def html_to_dict(html):
     table = bs(html, 'html.parser').find('div', attrs={'id' : 'score-data'}).text.split('\n')
     table[0] = table[0].split('!')[1]
     for row in table:
-        if (re.match('[А-ЯЁ]{3}', row)) and ('-:-' in row) and ('Перенесен' not in row):
-            row = re.split("(\d{2}:\d{2})|(d+')", row)
-            country = row[0]
-            if ("-:-" in row[3]) and ('[' in row[3]) and (']' in row[3]):
-                time = row[1]
-                teams = row[3].split('-:-')[0].strip().split(' - ')
+        try:
+            if (re.match('[А-ЯЁ]{3}', row)):
+                row = re.split("(\d{2}:\d{2})|(\d+')", row)
+                country = row[0]
+                print(row)
+                if ("-:-" in row[3]) and ('[' in row[3]) and (']' in row[3] and ('Перенесен' not in row[3])):
+                    time = row[1]
+                    teams = row[3].split('-:-')[0].strip().split(' - ')
+                    team1, team2 = teams[0], teams[1]
+                    kf = row[3].split('[')[-1][:-1].split('|')
+                    matches.append({
+                        "country": country,
+                        "time": time,
+                        "team1": team1,
+                        "team2": team2,
+                        "kw1": float(kf[0].strip()),
+                        "kx": float(kf[1].strip()),
+                        "kw2": float(kf[2].strip())
+                        })
+            elif ("-:-" in row) and ('[' in row) and (']' in row) and ('Перенесен' not in row):
+                time = row[:5]
+                teams = row.split('-:-')[0][5:].strip().split(' - ')
                 team1, team2 = teams[0], teams[1]
-                kf = row[3].split('[')[-1][:-1].split('|')
+                kf = row.split('[')[-1][:-1].split('|')
                 matches.append({
                     "country": country,
                     "time": time,
@@ -48,20 +64,8 @@ def html_to_dict(html):
                     "kx": float(kf[1].strip()),
                     "kw2": float(kf[2].strip())
                     })
-        elif ("-:-" in row) and ('[' in row) and (']' in row) and ('Перенесен' not in row):
-            time = row[:5]
-            teams = row.split('-:-')[0][5:].strip().split(' - ')
-            team1, team2 = teams[0], teams[1]
-            kf = row.split('[')[-1][:-1].split('|')
-            matches.append({
-                "country": country,
-                "time": time,
-                "team1": team1,
-                "team2": team2,
-                "kw1": float(kf[0].strip()),
-                "kx": float(kf[1].strip()),
-                "kw2": float(kf[2].strip())
-                })
+        except IndexError:
+            continue
     return matches
 
 def matches(data):
