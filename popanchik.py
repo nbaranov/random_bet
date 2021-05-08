84d4706bf3944ec5893d346fb8dec60296190bb4
Nikita Baranov <nbaranov.vrn@gmail.com>
2021-05-08 10:04:27 +0300

    minimal work version

--- popanchik.py
+++ popanchik.py
@@ -13,53 +13,58 @@ def returnMatchesForPopanchik(matches, hour, mink, maxk):
         if all([(mink <= line["kw1"] <= maxk or mink <= line["kw2"] <= maxk),
                 (int(line["time"][0:2]) >= hour),
                 not ((re.search(r"(\(Ж\))|(U\d{2})", line["team1"])) or (re.search(r"(\(Ж\))|(U\d{2})", line["team2"]))),
-                not (re.search(r"(товар)|(убок)|(рофей)", line["country"]))]):
+                not (re.search(r"(товар)|(убок)|(рофей)|(БРАЗИЛИЯ)", line["country"]))]):
             popanmatches.append(line)
     return popanmatches
 
 
-def getPopanPress(popanmatches):
-    press = []
-    coef = 1
-    a = 0
-    ligth_press = random.randint(2,3)
-    if ligth_press > (len(popanmatches) - len(usedmatches)):
-        print("Недостаточно попанских матчей на 2 пресса")
-        return 
-
-    while a < ligth_press:
-        i = random.randint(0, len(popanmatches) - 1)
-        if popanmatches[i] not in usedmatches:
-            usedmatches.append(popanmatches[i])
-            a += 1
-            if popanmatches[i]["kw1"] < popanmatches[i]["kw2"]:
-                press.append(f'{popanmatches[i]["country"]} {popanmatches[i]["time"]} \
+def getPopanPress(popanmatches, minCoefOfPress):
+    presses = []
+    while True:
+        coef = 1
+        usedmatches = []
+        press = []
+        while coef < minCoefOfPress:
+            if (len(popanmatches) - len(usedmatches) == 0):
+                print("Недостаточно попанских матчей на пресс")
+                return 
+            i = random.randint(0, len(popanmatches) - 1)
+            if popanmatches[i] not in usedmatches:
+                usedmatches.append(popanmatches[i])
+                if popanmatches[i]["kw1"] < popanmatches[i]["kw2"]:
+                    press.append(f'{popanmatches[i]["country"]} {popanmatches[i]["time"]} \
 {popanmatches[i]["team1"]} - {popanmatches[i]["team2"]} П1 кф. {popanmatches[i]["kw1"]}')
-                coef *= popanmatches[i]["kw1"]
-            else:
-                press.append(f'{popanmatches[i]["country"]} {popanmatches[i]["time"]} \
+                    coef *= popanmatches[i]["kw1"]
+                else:
+                    press.append(f'{popanmatches[i]["country"]} {popanmatches[i]["time"]} \
 {popanmatches[i]["team1"]} - {popanmatches[i]["team2"]} П2 кф. {popanmatches[i]["kw2"]}')
-                coef *= popanmatches[i]["kw2"]
+                    coef *= popanmatches[i]["kw2"]
+
+        sumOfBet = random.randint(1,4) * 50
+        
+        press.append(f"Ставка {sumOfBet} Итоговый кф {round(coef, 2)}")
+        
+        if press not in presses:
+            presses.append(press)
+        else:
+            for line in press:
+                print(line)
+            return press
 
 
-    press.append(f"Ставка  Итоговый кф {round(coef, 2)}")
-    return press
-
-
-def popanchik(popmatches):
+def popanchik(popmatches, minCoefOfPress):
     popanpress = []
 
     print(f'''Найден{ends(len(popmatches), "", "о", "о")} \
 {len(popmatches)} матч{ends(len(popmatches), "", "а", "ей")} для Попанчика ''')
     if len(popmatches) > 10:
         for _ in range(amt_preses):
-            press = getPopanPress(popmatches)
+            press = getPopanPress(popmatches, minCoefOfPress)
             if press == None: continue
             else: popanpress.append(press)
 
     return popanpress
 
 
-amt_preses = 2
-usedmatches = []
+amt_preses = 1
 
